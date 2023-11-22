9fb983a012930fa49aaf5f9f6f07b0532e25d40e
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2023-11-22 12:54:14 -0800

    actually update :trollface:

--- RemoteKeycard/Config.cs
+++ RemoteKeycard/Config.cs
@@ -1,55 +1,47 @@
-﻿using System.ComponentModel;
+﻿namespace RemoteKeycard;
+
+using System.ComponentModel;
 using Exiled.API.Interfaces;
 
-namespace RemoteKeycard
+public class Config : IConfig
 {
     /// <summary>
-    /// The plugin's main config file.
+    ///     Whether Amnesia affects the usage of keycards.
     /// </summary>
-    public class Config : IConfig
-    {
-        /// <inheritdoc/>
-        public bool IsEnabled { get; set; } = true;
+    [Description("Whether  Amnesia affects the usage of keycards.")]
+    public bool AmnesiaMatters { get; set; } = true;
 
-        /// <summary>
-        /// Gets whether debug messages should be shown.
-        /// </summary>
-        public bool Debug { get; set; } = false;
+    /// <summary>
+    ///     Whether this plugin works on generators.
+    /// </summary>
+    [Description("Whether this plugin works on generators.")]
+    public bool AffectGenerators { get; set; } = true;
 
-        /// <summary>
-        /// Whether Amnesia affects the usage of keycards.
-        /// </summary>
-        [Description("Whether  Amnesia affects the usage of keycards.")]
-        public bool AmnesiaMatters { get; set; } = true;
+    /// <summary>
+    ///     Whether this plugin works on Warhead's panel.
+    /// </summary>
+    [Description("Whether this plugin works on Warhead's panel.")]
+    public bool AffectWarheadPanel { get; set; } = true;
 
-        /// <summary>
-        /// Whether this plugin works on generators.
-        /// </summary>
-        [Description("Whether this plugin works on generators.")]
-        public bool AffectGenerators { get; set; } = true;
+    /// <summary>
+    ///     Whether this plugin works on SCP lockers.
+    /// </summary>
+    [Description("Whether this plugin works on SCP lockers.")]
+    public bool AffectScpLockers { get; set; } = true;
 
-        /// <summary>
-        /// Whether this plugin works on Warhead's panel.
-        /// </summary>
-        [Description("Whether this plugin works on Warhead's panel.")]
-        public bool AffectWarheadPanel { get; set; } = true;
+    /// <summary>
+    ///     Whether this plugin works on doors.
+    /// </summary>
+    [Description("Whether this plugin works on doors.")]
+    public bool AffectDoors { get; set; } = true;
 
-        /// <summary>
-        /// Whether this plugin works on SCP lockers.
-        /// </summary>
-        [Description("Whether this plugin works on SCP lockers.")]
-        public bool AffectScpLockers { get; set; } = true;
+    /// <summary>
+    ///     Gets whether exceptions should be shown.
+    /// </summary>
+    [Description("Toggle on/off exceptions/errors in console. (Enable this before reporting ANY bugs)")]
+    public bool ShowExceptions { get; set; } = false;
 
-        /// <summary>
-        /// Whether this plugin works on doors.
-        /// </summary>
-        [Description("Whether this plugin works on doors.")]
-        public bool AffectDoors { get; set; } = true;
+    public bool IsEnabled { get; set; } = true;
 
-        /// <summary>
-        /// Gets whether exceptions should be shown.
-        /// </summary>
-        [Description("Toggle on/off exceptions/errors in console. (Enable this before reporting ANY bugs)")]
-        public bool ShowExceptions { get; set; } = false;
-    }
+    public bool Debug { get; set; } = false;
 }
